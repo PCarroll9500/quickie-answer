a4ed1c5253ba430a2936557968fb910ecdb64a29
Patrick Carroll <pcarroll1@islander.tamucc.edu>
2022-05-07 16:14:50 -0500

    Query google 10 search results Search for Question the print the answer

--- main.py
+++ main.py
@@ -10,5 +10,17 @@ from googlesearch import search
 # Search for a question
 query = "Which of the following is the last step in the exchange between a web browser and a database?"
 
-for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-    print(j)
+
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0'}
+
+# For Loop to provide 10 google Query's
+for j in search(query, tld="co.in", num=10, stop=1, pause=2):
+    soup = BeautifulSoup(requests.get(j, headers=headers).content, 'html.parser')
+    for i, (question, answer) in enumerate(
+            zip(soup.select('a.SetPageTerm-wordText'), soup.select('a.SetPageTerm-definitionText')), 1):
+
+        # Look for the Question then respond with the Answer
+        if query in question.get_text(strip=True, separator='\n'):
+            print(answer.get_text(strip=True, separator='\n'))
+            break
+
